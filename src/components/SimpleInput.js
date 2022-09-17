e6b99eb24c182af543869bf622fbbdae39df4cc6
davidmgudeman <davidmgudeman@gmail.com>
2022-09-17 15:57:09 -0700

    commit

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,37 +1,51 @@
-import {useRef, useState} from 'react';
-
+import { useRef, useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
-const nameInputRef = useRef();
-const [enteredName, setEnteredName] = useState('');
-const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
-const  nameInputChangeHandler = event => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+  const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
+  };
 
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name Input is Valid");
     }
-const formSubmissionHandler = event => {
-  event.preventDefault();
-  if(enteredName.trim() === '') {
-    setEnteredNameIsValid(false);
-    return;
-    
-  }
-  setEnteredNameIsValid(true);
-  console.log(enteredName);
-  const enteredValue = nameInputRef.current.value;
-  console.log(enteredValue)
+  }, [enteredNameIsValid]);
 
-};
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    setEnteredNameTouched(true);
+    if (enteredName.trim() === "") {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredNameIsValid(true);
+    console.log(enteredName);
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+  };
 
-const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+  const nameInputClasses = !nameInputIsInvalid
+    ? "form-control"
+    : "form-control invalid";
 
   return (
     <form onSubmit={formSubmissionHandler}>
       <div className={nameInputClasses}>
-        <label htmlFor='name'>Your Name</label>
-        <input ref={nameInputRef} type='text' id='name' onChange={nameInputChangeHandler}/>
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+        />
       </div>
-      {!enteredNameIsValid && <p className="error-text">Name must not be empty</p>}
+      {nameInputIsInvalid && (<p className="error-text">Name must not be empty</p>)}
       <div className="form-actions">
         <button>Submit</button>
       </div>
